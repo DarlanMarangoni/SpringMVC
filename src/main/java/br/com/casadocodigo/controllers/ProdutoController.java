b7eef05228ed5ecd929c3c8d09d8a22e64f385d5
darlanmarangoni <marangoni.darlan1@gmail.com>
2019-06-18 11:57:12 -0300

    Configurando encoding do spring e mapeando classe ProdutoController

--- src/main/java/br/com/casadocodigo/controllers/ProdutoController.java
+++ src/main/java/br/com/casadocodigo/controllers/ProdutoController.java
@@ -13,26 +13,27 @@ import br.com.casadocodigo.models.Produto;
 import br.com.casadocodigo.models.enuns.TipoPreco;
 
 @Controller
+@RequestMapping("/produtos")
 public class ProdutoController {
 
 	@Autowired
 	private ProdutoDAO produtoDAO;
 	
-	@RequestMapping("/produtos/form")
+	@RequestMapping("/form")
 	public ModelAndView form() {
 		ModelAndView modelAndView = new ModelAndView("produtos/form");
 		modelAndView.addObject("tipos", TipoPreco.values());
 		return modelAndView;
 	}
 	
-	@RequestMapping(value="/produtos", method=RequestMethod.POST)
+	@RequestMapping(method=RequestMethod.POST)
 	public String save(Produto produto) {
 		System.out.println(produto);
 		produtoDAO.save(produto);
 		return "produtos/ok";
 	}
 	
-	@RequestMapping(value="/produtos", method=RequestMethod.GET)
+	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView listar() {
 		List<Produto> produtos = produtoDAO.listar();
 		ModelAndView modelAndView = new ModelAndView("produtos/lista");
